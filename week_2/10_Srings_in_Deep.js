734fa0b641d4cdce6b81e1f33d4245356ad367b3
Saqib Bedar <124094939+saqibbedar@users.noreply.github.com>
2023-12-21 23:55:03 +0500

    Update 10_Srings_in_Deep.js

--- week_2/10_Srings_in_Deep.js
+++ week_2/10_Srings_in_Deep.js
@@ -1,6 +1,6 @@
 // for loop over Arrays
 
-let arr = ['Saqib', 'Afsar']
+let arr = ['Saqib', 'Bedar']
 for (let i=0; i < arr.length; i++){
     console.log(arr[i])
 }
@@ -13,10 +13,10 @@ for (let i = 0; i < persons.length; i++){
 }
 // stings are not Arrays as their output is different from each other
 
-// We can use concat method to add two strings like we do using + operator
+// We can use the concat method to add two strings like we do using the + operator
 
 let greet = 'Hi' 
-let user = ' Afsar'
+let user = ' Saqib'
 
 console.log(greet + user); // adding with + operator
-console.log(greet.concat(user)); // Adding with .concat method
+console.log(greet.concat(user)); // Adding with .concat() method
